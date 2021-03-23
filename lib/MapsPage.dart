edb93209807dbd2a63b383fd4dd4f9c11e63512d
chirag <chiragaparadh@gmail.com>
2021-03-23 23:51:48 +0530

    Added getuserlocation method and updated api key.

--- lib/MapsPage.dart
+++ lib/MapsPage.dart
@@ -1,5 +1,6 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Maps extends StatefulWidget {
   @override
@@ -7,11 +8,28 @@ class Maps extends StatefulWidget {
 }
 
 class _MapsState extends State<Maps> {
+  double latitudedata;
+  double longitudedata;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
+
+  getCurrentLocation() async {
+    final geoposition = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      latitudedata = geoposition.latitude;
+      longitudedata = geoposition.longitude;
+    });
+  }
+
   GoogleMapController mapController;
-
-  //Hardcoded Latitude Longitude value, should be changed to user's current location.
-  final LatLng _center = const LatLng(19.112461, 72.933177);
-
+  //Hardcoded Latitude Longitude value, should be changed to user's current location. //implemented
+  // final LatLng _center = const LatLng(19.1125112, 72.93308);
   //List of markers for all the friends.
   Set<Marker> _markers = {};
 
@@ -21,19 +39,29 @@ class _MapsState extends State<Maps> {
     setState(() {
       _markers.add(Marker(
           markerId: MarkerId('val-1'),
-          position: _center,
+          position: LatLng(latitudedata, longitudedata),
           infoWindow: InfoWindow(title: 'My house', snippet: 'just my house')));
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    getCurrentLocation();
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.replay),
+        backgroundColor: Colors.red[800],
+        mini: true,
+        onPressed: () {
+          _onMapCreated(mapController);
+        },
+      ),
       body: GoogleMap(
+        zoomControlsEnabled: false,
         onMapCreated: _onMapCreated,
         markers: _markers,
         initialCameraPosition: CameraPosition(
-          target: _center,
+          target: LatLng(latitudedata, longitudedata),
           zoom: 11.0,
         ),
       ),

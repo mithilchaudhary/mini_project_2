38645d189cbefd7abdfe9f8e8e42ed1cfac933ed
chirag <chiragaparadh@gmail.com>
2021-03-24 12:15:22 +0530

    Added getAllFriends method, temp sample data created.

--- lib/MapsPage.dart
+++ lib/MapsPage.dart
@@ -8,13 +8,18 @@ class Maps extends StatefulWidget {
 }
 
 class _MapsState extends State<Maps> {
+  //user latitude, longitude
   double latitudedata;
   double longitudedata;
+
+  //List of markers for all the friends.
+  Set<Marker> _markers = {};
 
   @override
   void initState() {
     super.initState();
     getCurrentLocation();
+    getAllFriends();
   }
 
   getCurrentLocation() async {
@@ -27,26 +32,40 @@ class _MapsState extends State<Maps> {
     });
   }
 
-  GoogleMapController mapController;
-  //Hardcoded Latitude Longitude value, should be changed to user's current location. //implemented
-  // final LatLng _center = const LatLng(19.1125112, 72.93308);
-  //List of markers for all the friends.
-  Set<Marker> _markers = {};
+  getAllFriends() {
+    setState(() {
+      //Fake sample data, should be replaced with data provided by addFriendsPage(mithil).
+      _markers = {
+        Marker(
+            markerId: MarkerId('val-2'),
+            position: LatLng(19.11969264069933, 72.90916799688964),
+            infoWindow: InfoWindow(title: 'Nehal', snippet: 'just my house')),
+        Marker(
+            markerId: MarkerId('val-3'),
+            position: LatLng(19.102499403988425, 72.91723608145941),
+            infoWindow: InfoWindow(title: 'Mithil', snippet: 'just my house')),
+        Marker(
+            markerId: MarkerId('val-4'),
+            position: LatLng(19.120665789310753, 72.87861227234882),
+            infoWindow: InfoWindow(title: 'Hamaza', snippet: 'just my house'))
+      };
+    });
+  }
 
+  GoogleMapController mapController;
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
-    //Whenever new friend is added '_markers' set should be updated here.
+    //Setting user marker when map is created.
     setState(() {
       _markers.add(Marker(
           markerId: MarkerId('val-1'),
           position: LatLng(latitudedata, longitudedata),
-          infoWindow: InfoWindow(title: 'My house', snippet: 'just my house')));
+          infoWindow: InfoWindow(title: 'Chirag', snippet: 'home')));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    getCurrentLocation();
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.replay),

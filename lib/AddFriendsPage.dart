5402a92854bb72cd8656e807ffd3bc5d17f6d647
Mithil Chaudhary <mithil.chaudhary@yahoo.com>
2021-03-29 19:20:04 +0530

    hmmm

--- lib/AddFriendsPage.dart
+++ lib/AddFriendsPage.dart
@@ -8,6 +8,32 @@ class AddFriends extends StatefulWidget {
 class _AddFriendsState extends State<AddFriends> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final addFriendButton = Material(
+        elevation: 5,
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.cyan,
+        child: MaterialButton(
+          minWidth: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.fromLTRB(20, 15.0, 20.0, 15.0),
+          onPressed: () {},
+          child: Text(
+            "Add a new Friend",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 13),
+          ),
+        ));
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: addFriendButton,
+          )
+        ],
+      ),
+    );
   }
 }
